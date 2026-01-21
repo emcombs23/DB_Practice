4f10dbedc4f3adb6a9860587a8cc4ad9afd61d49
emcombs23 <emcombs.23.sv@gmail.com>
2026-01-21 15:49:18 +0000

    day 3

--- fetch_teams.py
+++ fetch_teams.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 conn = sqlite3.connect('playoffs.db')
 cursor = conn.cursor()
@@ -19,3 +20,23 @@ conn.close()
 
 for item in result:
     print(item)
+
+df = pd.DataFrame(result, columns=['id', 'City', 'Name'])
+print(df)
+print(df['City'])
+
+cityList = []
+for item in result:
+    cityList.append(item[1])
+print(cityList)
+
+cityLengthList = []
+for item in result:
+    cityLengthList.append(len(item[1]))
+print(cityLengthList)
+
+
+teamLengthList = []
+for item in result:
+    teamLengthList.append((len(item[1]), len(item[2])))
+print(teamLengthList)
